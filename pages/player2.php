e7707d5a33f1809719c78d0afb6e8528b34219c7
MrOrangeJuice <parrottoalex@gmail.com>
2020-06-30 17:39:30 -0400

    Fixed product hovers

--- pages/player2.php
+++ pages/player2.php
@@ -1,6 +1,6 @@
 <div class="page">
 	<div>
-		<a href="player2app.com"><img src="media/P2 Banner Red & Navy.png" alt="Player 2 Banner" class="productBanner productPage"></a>
+		<a href="http://www.player2app.com"><img src="media/P2 Banner Red & Navy.png" alt="Player 2 Banner" class="productBanner productPage"></a>
 		<p>Co-developed with our partners at JumpButton Studio, Dynasty 11 Studios is currently hard at work on our first app for iOS and Android called Player 2. The app is a social networking and team building platform for gamers, to be released in 2020. Follow us on social media for further details!</p>
 		<form action="https://mailchi.mp/8290b199e31b/dynasty-11-mailing-list-signup">
 			<button class="bigButton">Join the Mailing List</button>
